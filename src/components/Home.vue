d338ea6a47ddc6cfae6f7483bc784f6207b1067f
Brance Boren <branceboren@gmail.com>
2017-11-14 21:36:13 -0700

    Generated project and removed fluff

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Burrow</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+</style>
